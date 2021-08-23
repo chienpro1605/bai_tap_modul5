e20103193508b67d33633f2e4feb591ade693fd0
chienpro1605 <leducchien468@gmail.com>
2021-08-23 11:12:16 +0700

    bai_tap_5

---
+++ angular-template-form/src/app/template-form-student/template-form-student.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-template-form-student',
+  templateUrl: './template-form-student.component.html',
+  styleUrls: ['./template-form-student.component.css']
+})
+export class TemplateFormStudentComponent implements OnInit {
+  student = {
+    studentCode: 'SV-002',
+    studentName: 'Nguyễn Văn B',
+    studentAddress: 'Nghệ An'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(createStudentForm: NgForm) {
+    // console.log(createStudentForm.value);
+    console.log(this.student);
+  }
+}
